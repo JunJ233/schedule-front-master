01b12d4c74237268182c60a42786cdcc57b4fc4b
xujunjun <904752171@qq.com>
2023-01-19 19:17:39 +0800

    first commit

---
+++ .history/src/main_20230119191711.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import mainVue from './components/mainVue'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router: router,
+}).$mount('#app')
+
+new Vue({
+  render:h =>h(mainVue)
+}).$mount(#mainVue')
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  margin: 0px;
+}
+.centerFlex(@dirc: column){ 
+  display: flex;
+  flex-direction: @dirc;
+  justify-content: center;
+  align-items: center;
+}
+.mainBox{
+  width: 100vw;
+  height: 100vh;
+  .centerFlex(row)
+}
+.halfVBox{
+  width: 50vw;
+  height: 100vh;
+  .centerFlex
+}
+
+</style>
